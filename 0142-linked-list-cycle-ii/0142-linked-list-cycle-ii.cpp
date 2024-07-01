7491f210b8e244d54444b7ba95fdadefca31b422
Priyanshu <pshrivastava403@outlook.com>
2024-07-01 04:13:56 -0400

    Time: 4 ms (86.13%), Space: 10.2 MB (19.64%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -6,20 +6,46 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// hastset based solution
+// class Solution {
+// public:
+//     ListNode *detectCycle(ListNode *head) {
+//         unordered_set<ListNode*> hSet;
+//         ListNode* curr = head;
+//         while(curr != nullptr) {
+//             if(hSet.find(curr) != hSet.end()) {
+//                 return curr;
+//             }
+//             else {
+//                 hSet.insert(curr);
+//             }
+//             curr = curr->next;
+//         }
+//         return nullptr;
+//     }
+// };
+
+// slow and fast pointer based solution
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        unordered_set<ListNode*> hSet;
-        ListNode* curr = head;
-        while(curr != nullptr) {
-            if(hSet.find(curr) != hSet.end()) {
-                return curr;
+        if(head == nullptr) return nullptr;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(fast == slow) {
+                break;
             }
-            else {
-                hSet.insert(curr);
-            }
-            curr = curr->next;
         }
-        return nullptr;
+        if(fast == nullptr || fast->next == nullptr) return nullptr;
+        slow = head;
+        while(slow != fast) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        return slow;
     }
 };
