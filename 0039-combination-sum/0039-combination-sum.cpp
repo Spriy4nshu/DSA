550c529e0a956b726acaf6a601cc0e92d714aa76
Priyanshu <pshrivastava403@outlook.com>
2024-10-26 14:12:22 -0400

    Time: 83 ms (8.72%), Space: 68.6 MB (8.35%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        vector<int> path;
+        helper(candidates, 0, target, path, result);
+        return result;
+    }
+    
+private:
+    void helper(vector<int>& candidates, int idx, int target, vector<int> path, vector<vector<int>>& result) {
+        // base cases
+        if (idx == candidates.size() || target < 0) return;
+        if (target == 0) {
+            result.push_back(path);
+            return;
+        }
+        
+        // logic
+        // not choose current number
+        helper(candidates, idx + 1, target, path, result);
+        
+        // choose current number
+        path.push_back(candidates[idx]);
+        helper(candidates, idx, target - candidates[idx], path, result);
+    }
+};
