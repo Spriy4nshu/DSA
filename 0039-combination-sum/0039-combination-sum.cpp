546d02913b15e300d250aaa6675c91e11d0541d4
Priyanshu <pshrivastava403@outlook.com>
2024-10-26 16:11:19 -0400

    Time: 2 ms (60.86%), Space: 12.6 MB (56.5%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,33 +1,32 @@
-class Solution {
-public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> result;
-        vector<int> path;
-        helper(candidates, 0, target, path, result);
-        return result;
-    }
+// class Solution {
+// public:
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//         vector<vector<int>> result;
+//         vector<int> path;
+//         helper(candidates, 0, target, path, result);
+//         return result;
+//     }
     
-private:
-    void helper(vector<int>& candidates, int idx, int target, vector<int> path, vector<vector<int>>& result) {
-        // base cases
-        if (idx >= candidates.size() || target < 0) return;
-        if (target == 0) {
-            result.push_back(path);
-            return;
-        }
+// private:
+//     void helper(vector<int>& candidates, int idx, int target, vector<int> path, vector<vector<int>>& result) {
+//         // base cases
+//         if (idx >= candidates.size() || target < 0) return;
+//         if (target == 0) {
+//             result.push_back(path);
+//             return;
+//         }
         
-        // logic
-        // dont choose
-        helper(candidates, idx + 1, target, path, result);
+//         // logic
+//         // dont choose
+//         helper(candidates, idx + 1, target, path, result);
 
-        // choose
-        vector<int> li = path;
-        //action
-        li.push_back(candidates[idx]);
-        // recurse
-        helper(candidates, idx, target - candidates[idx], li, result);
-    }
-};
+//         // choose
+//         //action
+//         path.push_back(candidates[idx]);
+//         // recurse
+//         helper(candidates, idx, target - candidates[idx], path, result);
+//     }
+// };
 
 // class Solution {
 // public:
@@ -90,32 +89,32 @@ private:
 //     }
 // };
 
-// class Solution {
-// public:
-//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-//         vector<vector<int>> result;
-//         vector<int> path;
-//         helper(candidates, 0, target, path, result);
-//         return result;
-//     }
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        vector<int> path;
+        helper(candidates, 0, target, path, result);
+        return result;
+    }
     
-// private:
-//     void helper(vector<int>& candidates, int pivot, int target, vector<int>& path, vector<vector<int>>& result) {
-//         // base cases
-//         if (target < 0) return;
-//         if (target == 0) {
-//             result.push_back(path);
-//             return;
-//         }
+private:
+    void helper(vector<int>& candidates, int pivot, int target, vector<int>& path, vector<vector<int>>& result) {
+        // base cases
+        if (target < 0) return;
+        if (target == 0) {
+            result.push_back(path);
+            return;
+        }
         
-//         // logic
-//         for(int i = pivot; i < candidates.size(); i++){
-//             // action
-//             path.push_back(candidates[i]);
-//             // recurse 
-//             helper(candidates, i, target - candidates[i], path, result);
-//             // backtrack
-//             path.pop_back();
-//         }
-//     }
-// };
+        // logic
+        for(int i = pivot; i < candidates.size(); i++){
+            // action
+            path.push_back(candidates[i]);
+            // recurse 
+            helper(candidates, i, target - candidates[i], path, result);
+            // backtrack
+            path.pop_back();
+        }
+    }
+};
