33e0bd2a7849c8b2a2f9c1f57cd418a083c2ac9e
Priyanshu <pshrivastava403@outlook.com>
2024-10-26 16:10:13 -0400

    Time: 114 ms (5.37%), Space: 85 MB (5%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -8,19 +8,114 @@ public:
     }
     
 private:
-    void helper(vector<int>& candidates, int pivot, int target, vector<int>& path, vector<vector<int>>& result) {
+    void helper(vector<int>& candidates, int idx, int target, vector<int> path, vector<vector<int>>& result) {
         // base cases
-        if (pivot == candidates.size() || target < 0) return;
+        if (idx >= candidates.size() || target < 0) return;
         if (target == 0) {
             result.push_back(path);
             return;
         }
         
         // logic
-        for(int i = pivot; i < candidates.size(); i++){
-            path.push_back(candidates[i]);
-            helper(candidates, i, target - candidates[i], path, result);
-            path.pop_back();
-        }
+        // dont choose
+        helper(candidates, idx + 1, target, path, result);
+
+        // choose
+        vector<int> li = path;
+        //action
+        li.push_back(candidates[idx]);
+        // recurse
+        helper(candidates, idx, target - candidates[idx], li, result);
     }
 };
+
+// class Solution {
+// public:
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//         vector<vector<int>> result;
+//         vector<int> path;
+//         helper(candidates, 0, target, path, result);
+//         return result;
+//     }
+    
+// private:
+//     void helper(vector<int>& candidates, int idx, int target, vector<int>& path, vector<vector<int>>& result) {
+//         // base cases
+//         if (idx >= candidates.size() || target < 0) return;
+//         if (target == 0) {
+//             result.push_back(path);
+//             return;
+//         }
+        
+//         // logic
+//         // dont choose
+//         helper(candidates, idx + 1, target, path, result);
+
+//         // choose
+//         //action
+//         path.push_back(candidates[idx]);
+//         // recurse
+//         helper(candidates, idx, target - candidates[idx], path, result);
+//         // backtrack
+//         path.pop_back();
+//     }
+// };
+
+// class Solution {
+// public:
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//         vector<vector<int>> result;
+//         vector<int> path;
+//         helper(candidates, 0, target, path, result);
+//         return result;
+//     }
+    
+// private:
+//     void helper(vector<int>& candidates, int pivot, int target, vector<int> path, vector<vector<int>>& result) {
+//         // base cases
+//         if (target < 0) return;
+//         if (target == 0) {
+//             result.push_back(path);
+//             return;
+//         }
+        
+//         // logic
+//         for(int i = pivot; i < candidates.size(); i++){
+//             vector<int> li = path;
+//             //action
+//             li.push_back(candidates[i]);
+//             // recurse
+//             helper(candidates, i, target - candidates[i], li, result);
+//         }
+//     }
+// };
+
+// class Solution {
+// public:
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//         vector<vector<int>> result;
+//         vector<int> path;
+//         helper(candidates, 0, target, path, result);
+//         return result;
+//     }
+    
+// private:
+//     void helper(vector<int>& candidates, int pivot, int target, vector<int>& path, vector<vector<int>>& result) {
+//         // base cases
+//         if (target < 0) return;
+//         if (target == 0) {
+//             result.push_back(path);
+//             return;
+//         }
+        
+//         // logic
+//         for(int i = pivot; i < candidates.size(); i++){
+//             // action
+//             path.push_back(candidates[i]);
+//             // recurse 
+//             helper(candidates, i, target - candidates[i], path, result);
+//             // backtrack
+//             path.pop_back();
+//         }
+//     }
+// };
