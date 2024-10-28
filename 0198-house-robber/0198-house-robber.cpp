c233feec5f54db1d3a809b6ea1903433a7e56467
Priyanshu <pshrivastava403@outlook.com>
2024-10-28 07:34:26 -0400

    Time: 0 ms (100%), Space: 9.4 MB (62.89%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,45 +1,45 @@
 // //DP Approach: variable
 
-// class Solution {
-// public:
-//     int rob(vector<int>& nums) {
-//         if(nums.size() == 1) return nums[0];
-//         int a1 = nums[0];
-//         int a2 = max(a1, nums[1]);
-//         for(int i = 2; i < nums.size(); i++){
-//             if(nums[i] + a1 > a2){
-//                 int temp = a1;
-//                 a1 = a2;
-//                 a2 = nums[i] + temp;
-//             }
-//             else{
-//                 a1 = a2;
-//             }
-//         }
-//         return max(a1, a2);
-//     }
-// };
-
-// DP Approach: 1D array
-
 class Solution {
 public:
     int rob(vector<int>& nums) {
         if(nums.size() == 1) return nums[0];
-        vector<int> robbings;
-        robbings.push_back(nums[0]);
-        robbings.push_back(max(nums[1], nums[0]));
+        int a1 = nums[0];
+        int a2 = max(a1, nums[1]);
         for(int i = 2; i < nums.size(); i++){
-            if(nums[i] + robbings[i - 2] > robbings[i - 1]){
-                robbings.push_back(nums[i] + robbings[i - 2]);
+            if(nums[i] + a1 > a2){
+                int temp = a1;
+                a1 = a2;
+                a2 = nums[i] + temp;
             }
             else{
-                robbings.push_back(robbings[i - 1]);
+                a1 = a2;
             }
         }
-        return robbings[nums.size() - 1];
+        return max(a1, a2);
     }
 };
+
+// DP Approach: 1D array
+
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         if(nums.size() == 1) return nums[0];
+//         vector<int> robbings;
+//         robbings.push_back(nums[0]);
+//         robbings.push_back(max(nums[1], nums[0]));
+//         for(int i = 2; i < nums.size(); i++){
+//             if(nums[i] + robbings[i - 2] > robbings[i - 1]){
+//                 robbings.push_back(nums[i] + robbings[i - 2]);
+//             }
+//             else{
+//                 robbings.push_back(robbings[i - 1]);
+//             }
+//         }
+//         return robbings[nums.size() - 1];
+//     }
+// };
 
 
 // Exhaustive Approach:
