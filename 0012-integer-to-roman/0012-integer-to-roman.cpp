cbc9a285da89c250ab1b538677c7a4f015bb7142
Priyanshu <pshrivastava403@outlook.com>
2024-11-09 16:56:26 -0500

    Time: 2 ms (74.49%), Space: 13.1 MB (40.93%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,3 +1,21 @@
+// class Solution {
+// public:
+//     string intToRoman(int num) {
+//         vector<int> base = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+//         vector<string> roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+//         string res = "";
+//         // using subtraction method
+//         for(int i = 0; i < base.size(); i++){
+//             while(num >= base[i]){
+//                 res.append(roman[i]);
+//                 num -= base[i];
+//             }
+//         }
+//         return res;
+//     }
+// };
+
+// using division method
 class Solution {
 public:
     string intToRoman(int num) {
@@ -6,9 +24,14 @@ public:
         string res = "";
         // using subtraction method
         for(int i = 0; i < base.size(); i++){
-            while(num >= base[i]){
-                res.append(roman[i]);
-                num -= base[i];
+            if(num >= base[i]){
+                int recur = num / base[i];
+                while(recur > 0){
+                    res.append(roman[i]);
+                    recur--;
+                }
+
+                num = num % base[i];
             }
         }
         return res;
