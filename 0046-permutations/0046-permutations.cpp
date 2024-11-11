e8c13461dfd0397f8c4055af37cc7538c2a77dd0
Priyanshu <pshrivastava403@outlook.com>
2024-11-11 16:46:23 -0500

    Time: 0 ms (100%), Space: 9.2 MB (58.69%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -29,40 +29,76 @@
 //     }
 // };
 
+// class Solution {
+// private:
+//     vector<vector<int>> res;
+//     void helper(vector<int>& nums, vector<bool>& exist, vector<int>& path) {
+//         // base case
+//         if (path.size() == nums.size()) {
+//             res.push_back(path);
+//             return;
+//         }
+
+//         // logic
+//         for (int i = 0; i < nums.size(); i++) {
+//             // skip if number is already used
+//             if (exist[i]) continue;
+            
+//             // action
+//             exist[i] = true;
+//             path.push_back(nums[i]);
+            
+//             // recurse
+//             helper(nums, exist, path);
+            
+//             // backtrack
+//             path.pop_back();
+//             exist[i] = false;
+//         }
+//     }
+    
+    
+// public:
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         if (nums.size() == 0) return {};
+//         vector<int> path;
+//         vector<bool> exist(nums.size(), false);  // initialize with false
+//         helper(nums, exist, path);
+//         return res;
+//     }
+// };
+
+
 class Solution {
 private:
-    vector<vector<int>> res;
-    void helper(vector<int>& nums, vector<bool>& exist, vector<int>& path) {
-        // base case
-        if (path.size() == nums.size()) {
+    void helper(vector<int>& nums, vector<vector<int>>& res, int idx, vector<int>& path, vector<int>& visited){
+        // base
+        if(idx > nums.size()) return;
+        if(path.size() == nums.size()){
             res.push_back(path);
             return;
         }
-
-        // logic
-        for (int i = 0; i < nums.size(); i++) {
-            // skip if number is already used
-            if (exist[i]) continue;
-            
+        // for loop based recursion
+        for(int i = 0; i < nums.size(); i++){
+            if(visited[i]) continue;
             // action
-            exist[i] = true;
             path.push_back(nums[i]);
-            
-            // recurse
-            helper(nums, exist, path);
-            
-            // backtrack
+            visited[i] = 1;
+            //recurse
+            helper(nums, res, i, path, visited);
+            //backtrack
             path.pop_back();
-            exist[i] = false;
+            visited[i] = 0;
         }
     }
     
+    
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        if (nums.size() == 0) return {};
+        vector<vector<int>> res;
         vector<int> path;
-        vector<bool> exist(nums.size(), false);  // initialize with false
-        helper(nums, exist, path);
+        vector<int> visited(nums.size(), 0);
+        helper(nums, res, 0, path, visited);
         return res;
     }
 };
